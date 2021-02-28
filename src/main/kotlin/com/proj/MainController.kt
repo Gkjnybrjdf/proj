b92c575701f61c8f236d5c9dd37a3520910ee525
Ann Gkjnybrjdf <sneginaannett@gmail.com>
2021-02-28 20:17:14 +0300

    added CrossOrigin

--- src/main/kotlin/com/proj/MainController.kt
+++ src/main/kotlin/com/proj/MainController.kt
@@ -5,6 +5,7 @@ import com.proj.model.OutputMessage
 import com.proj.service.HighlightService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController("/")
 class MainController(var highlightService: HighlightService) {
 
+    @CrossOrigin
     @PostMapping
     fun post(@RequestBody inputMessage: InputMessage)
     : OutputMessage {
