35bdd79fc9de33779379b33db6e213b1db385530
JerrySly <52193061+JerrySly@users.noreply.github.com>
2021-02-28 15:48:17 +0300

    safe commit

--- front/src/views/NavContent.vue
+++ front/src/views/NavContent.vue
@@ -1,4 +1,22 @@
 <template>
+    <div>
+        <v-row
+        align="center"
+        justify="space-around"
+        class="row-login">
+            <v-avatar
+            color="primary"
+            size="50"
+            class="avatar"
+            ></v-avatar>
+            <v-btn
+            color="primary"
+            elevation="2" 
+            class="login"
+            >LogIn/SingUp</v-btn>
+        </v-row>
+        <v-divider class="splitter"></v-divider>
+    </div>
 </template>
 
 <script>
@@ -8,5 +26,14 @@ export default {
 </script>
 
 <style>
-
+.splitter{
+    margin-top:2%;
+    margin-bottom:2%;
+}
+.avatar{
+    margin:2%;
+}
+.row-login{
+    height:100px;
+}
 </style>
