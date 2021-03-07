a000772507476729cb3ee6355228b7eec0a667d1
a.plotnikova <sneginaannet@gmail.com>
2021-03-07 12:45:01 +0300

    deploy

--- src/main/kotlin/com/proj/MainController.kt
+++ src/main/kotlin/com/proj/MainController.kt
@@ -5,19 +5,25 @@ import com.proj.model.OutputMessage
 import com.proj.service.HighlightService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
-@RestController("/")
+@RestController("/message")
 class MainController(var highlightService: HighlightService) {
 
     @PostMapping
     fun post(@RequestBody inputMessage: InputMessage)
     : OutputMessage {
         return highlightService.getOutput(inputMessage)
+    }
+}
+
+@Controller("/")
+class IndexController {
+    @GetMapping
+    fun get() : String {
+        return "index"
     }
 }
 
