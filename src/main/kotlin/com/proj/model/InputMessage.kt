3984a128b1d6c4495c38f3d6a607f2eab45f7740
a.plotnikova <sneginaannet@gmail.com>
2021-03-28 14:04:47 +0300

    Добавилась модель TextRank

--- src/main/kotlin/com/proj/model/InputMessage.kt
+++ src/main/kotlin/com/proj/model/InputMessage.kt
@@ -1,7 +1,7 @@
 package com.proj.model
 
 enum class Type {
-    Tf, Tfidf, Lda
+    Tf, Tfidf, Lda, TextRank
 }
 
 class InputMessage(var id: String, var input_text: String, var sentence_count : Long, var type: Type)
